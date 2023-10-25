e00d845ce67ffefc20632e89248dc6085963f9a0
Romain.jhr <romain.jahier@edu.esiee-it.fr>
2023-10-25 09:52:21 +0200

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login";
 import Signup from "./pages/signup";
+import MenuHamburger from "./components/MenuHamburger";
 
 function App(): React.ReactElement {
   return (
     <div className="App">
       <Router>
+      <MenuHamburger />
         <Routes>
-          <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
     </div>
