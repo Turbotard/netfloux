8afc014fbe5d0e96994247d788bdc2f66465011d
Turbotard <turbotard9@gmail.com>
2023-10-25 10:18:02 +0200

    update profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Auth, getAuth, onAuthStateChanged, signOut, User } from "firebase/auth";
+import {
+  Auth,
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+  User,
+} from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 const Profile: React.FC = () => {
@@ -8,17 +14,15 @@ const Profile: React.FC = () => {
   const authInstance: Auth = getAuth();
 
   useEffect(() => {
-    // Écoute les changements d'état d'authentification
     const unsubscribe = onAuthStateChanged(authInstance, (currentUser) => {
       if (currentUser) {
         setUser(currentUser);
       } else {
         setUser(null);
-        navigate("/"); // Redirige vers la page de connexion si déconnecté
+        navigate("/");
       }
     });
 
-    // Nettoie l'écouteur lors de la désinscription du composant
     return () => unsubscribe();
   }, [navigate, authInstance]);
 
@@ -41,6 +45,6 @@ const Profile: React.FC = () => {
       <button onClick={handleSignOut}>Déconnexion</button>
     </div>
   );
-}
+};
 
 export default Profile;
