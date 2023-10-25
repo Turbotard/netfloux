52a99c1f89b36add50f8fabc5fe2a705bae19725
Romain.jhr <romain.jahier@edu.esiee-it.fr>
2023-10-25 09:59:06 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login";
 import Signup from "./pages/signup";
-import MenuHamburger from "./components/MenuHamburger";
+import MenuHamburger from "./components/Navbar";
 
 function App(): React.ReactElement {
   return (
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import LoginIcon from '@mui/icons-material/Login';
+import SignupIcon from '@mui/icons-material/PersonAdd';
+import HomeIcon from '@mui/icons-material/Home';
+import ProfileIcon from '@mui/icons-material/Person';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const Routes = [
+    { path: '', icon: <HomeIcon />, name: 'Home' },
+    { path: 'login', icon: <LoginIcon />, name: 'Log in' },
+    { path: 'signup', icon: <SignupIcon />, name: 'Signup' },
+    { path: 'profil/:id', icon: <ProfileIcon />, name: 'Profil Utilisateur' },
+  ];
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        {Routes.map((route) => (
+          <Button
+            key={route.name}
+            startIcon={route.icon}
+            component={Link}
+            to={`/${route.path}`}
+            color="inherit"
+          >
+            {route.name}
+          </Button>
+        ))}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
