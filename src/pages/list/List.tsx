391da4ddabb3852b03254515ca196037a2a550ce
EstebanDardillac <110052260+EstebanDardillac@users.noreply.github.com>
2023-10-26 11:21:08 +0200

    upgrade liste des series

--- src/pages/list/List.tsx
+++ src/pages/list/List.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ListWithFilters from '../../components/listWithFilters/ListWithFilters';
-import { Typography } from '@mui/material';
+import { Box, ThemeProvider, Typography, createTheme } from '@mui/material';
 import PopularSeriesCorridor from '../../components/seriesCorridor/PopularSeriesCorridor';
+import './list.css';
 
 const ListPage: React.FC = () => {
+    const defaultTheme = createTheme();
+
+
     return (
-        <div>
-            <Typography variant="h4" gutterBottom>
+        <ThemeProvider theme={defaultTheme}>
+        
+
+            <Box className="populaires">
+                <Typography variant="h4" className='titre'>
+                    Les Plus Populaires:
+                </Typography>
                 <PopularSeriesCorridor />
-            </Typography>
-            <Typography variant="h4" gutterBottom style={{marginTop: '20px'}}>
-                Liste avec filtres:
-            </Typography>
+            </Box>
             <ListWithFilters />
-        </div>
+        </ThemeProvider>
+
     );
 }
 
