7adf4ece9635c937c649a801355f6fa9daccd6df
EstebanDardillac <110052260+EstebanDardillac@users.noreply.github.com>
2023-10-26 23:51:26 +0200

    changement navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
+import { Typography, Box, createTheme } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import HomeIcon from '@mui/icons-material/Home';
 import ProfileIcon from '@mui/icons-material/Person';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './navbar.css';
 
 import ListIcon from '@mui/icons-material/List';
 
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () =>{
+    navigate("/list")
+  }
   const Routes = [
-    { path: '', icon: <HomeIcon />, name: 'Home' },
     { path: 'profile', icon: <ProfileIcon />, name: 'Profil Utilisateur' },
     { path: 'list', icon: <ListIcon />, name: 'List' },
   ];
 
   return (
     <AppBar position="static" >
-      <Toolbar>
+      <Toolbar className='navbar'>
+        <Button 
+        onClick={handleNavigate}
+        >
+          <Box className="logo">
+            <img src='/img/o.png'></img>
+          </Box>
+          
+        </Button>
         {Routes.map((route) => (
           <Button
             key={route.name}
