79782a0c43cf1540e0034bd239a0f57c7eb6fb5f
EstebanDardillac <110052260+EstebanDardillac@users.noreply.github.com>
2023-10-27 10:33:33 +0200

    suivis css

--- src/pages/suivis/suivis.tsx
+++ src/pages/suivis/suivis.tsx
@@ -16,19 +16,27 @@ import {
   createTheme,
   Grid,
   Rating,
+  DialogContent,
+  DialogActions,
+  Dialog,
+  DialogTitle,
 } from "@mui/material";
 import { doc, getDoc, arrayRemove } from "firebase/firestore";
 import { firestore } from "../../db/db";
 import { Auth, User, getAuth, onAuthStateChanged } from "@firebase/auth";
 import Navbar from "../../components/navbar/Navbar";
+import './suivis.css'
 
 const Suivis: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
   const [series, setSeries] = useState<Show[]>([]);
   const [page, setPage] = useState<number>(1);
   const defaultTheme = createTheme();
-  const authInstance: Auth = getAuth();
   const [ratingValue, setRatingValue] = useState<number>(0);
+  const [open, setOpen] = useState<boolean>(false);
+  const [selectedSeries, setSelectedSeries] = useState<Show | null>(null);
+  const authInstance: Auth = getAuth();
+
   const handleRatingChange = (
     event: React.ChangeEvent<{}>,
     newValue: number | null
@@ -73,47 +81,128 @@ const Suivis: React.FC = () => {
         );
       }
     }
+
+  };
+  const handleOpen = async (serie: any) => {
+    if (user) {
+      const docId = `${user.uid}_${serie.id}`;
+      const evalDocRef = doc(firestore, "eval", docId);
+      const docSnap = await getDoc(evalDocRef);
+
+      if (docSnap.exists()) {
+        setRatingValue(docSnap.data().rating);
+      } else {
+        setRatingValue(0);
+      }
+    }
+
+    setSelectedSeries(serie);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Navbar />
       <Grid className="background">
-        {series.map((serie, index) => (
-          <Card key={index} style={{ maxWidth: "300px" }} className="card">
-            <CardMedia
-              component="img"
-              alt={serie.title}
-              height="auto"
-              width="70%"
-              image={serie.poster}
-            />
-            <CardContent className="card-description">
-              <Typography className="title">{serie.title}</Typography>
+        <Typography variant="h4" className='titre'>
+          Vos series et films favoris:
+        </Typography>
+        <Box display="flex" flexWrap="wrap" gap={2}>
+          {series.map((serie, index) => (
+            <Card 
+            key={index} 
+            style={{ maxWidth: "300px" }}
+            onClick={() => handleOpen(serie)}
+             className="card">
+              <CardMedia
+                component="img"
+                alt={serie.title}
+                height="auto"
+                width="70%"
+                className="card"
+                image={serie.poster}
+              />
+              <CardContent className="card-description">
+                <Typography className="title">{serie.title}</Typography>
+                <Typography variant="subtitle1">
+                  Genres:
+                  <Box className="card-d">{serie.genres.join(", ")}</Box>
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  className="button-delete"
+                  onClick={() => handleRemoveFromFavorites(serie.title)}
+                >
+                  Supprimer des favoris
+                </Button>
+              </CardContent>
+            </Card>
+          ))}
+        </Box>
+
+        <Dialog open={open} onClose={handleClose} >
+        {selectedSeries && (
+          <>
+          <Grid  className="background-dia">
+          <Box className="fav">
+              <DialogTitle >{selectedSeries.title}</DialogTitle>
+
+            </Box>
+
+            <DialogContent >
+              <CardMedia
+                component="img"
+                alt={selectedSeries.title}
+                height="auto"
+                width="70%"
+                image={selectedSeries.poster}
+              />
+              <Box className="details">
+              <Typography variant="h6">{selectedSeries.title}</Typography>
               <Typography variant="subtitle1">
-                Genres:
-                <Box className="card-d">{serie.genres.join(", ")}</Box>
+                {selectedSeries.synopsis}
               </Typography>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => handleRemoveFromFavorites(serie.title)}
-              >
-                Supprimer des favoris
+              <Typography variant="subtitle2">
+                Acteurs: {selectedSeries.actors?.join(", ")}
+              </Typography>
+              </Box>
+              
+              <Box component="fieldset" borderColor="#343434">
+                <Typography component="legend">Noter cette série:</Typography>
+                <Rating
+                  name="rating-value"
+                  value={ratingValue}
+                  onChange={handleRatingChange}
+                  className="note"
+                />
+              </Box>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} className="button-all">
+                Fermer
               </Button>
-            </CardContent>
-          </Card>
-        ))}
+            </DialogActions>
+          </Grid>
+           
+          </>
+        )}
+      </Dialog>
+
         <Box mt={3} display="flex" justifyContent="center">
           <Button
             onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
-            className="button"
+            className="button-suivis"
           >
             Précédent
           </Button>
           <Button
             onClick={() => setPage((prev) => prev + 1)}
-            className="button"
+            className="button-suivis"
           >
             Suivant
           </Button>
